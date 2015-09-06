1a750e454d2ce40df4582ac0d01185d90f8a576f
Imtaek Hong <imtaekh@gmail.com>
2015-09-06 15:46:04 -0700

    marker setup

--- gulp/js/controllers/MapController.js
+++ gulp/js/controllers/MapController.js
@@ -8,27 +8,60 @@
   function MapController($rootScope, $window, Auth){
     var vm = this;
 
-    var centerLatLng ={lat:34.05223,lng:-118.24368}
-    window.navigator.geolocation.getCurrentPosition(
-      function (position) {
-        centerLatLng.lat = position.coords.latitude;
-        centerLatLng.lng = position.coords.longitude;
-        console.log(position);
-        var mapOptions = {
-                      zoom: 15,
-                      center: new google.maps.LatLng(centerLatLng.lat,centerLatLng.lng)
-                  };
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      },function (err) {
-        console.log(err);
-        var mapOptions = {
-                      zoom: 15,
-                      center: new google.maps.LatLng(centerLatLng.lat,centerLatLng.lng)
-                  };
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-      }
-    );
+    vm.markerData={
+      lat:34.052,
+      lng:-118.243,
+      imgUrl:"http://www.keenthemes.com/preview/metronic/theme/assets/global/plugins/jcrop/demos/demo_files/image1.jpg"
+    };
+
+    var centerLatLng = {lat:34.05223,lng:-118.24368};
     document.getElementById('map').style.height=window.innerHeight-50+"px";
+    var mapInit = function () {
+      var mapOptions = {
+                    zoom: 15,
+                    center: new google.maps.LatLng(centerLatLng.lat,centerLatLng.lng)
+                };
+      vm.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+      vm.marker = new google.maps.Marker({
+                          // map: vm.map,
+                          position: new google.maps.LatLng(vm.markerData.lat, vm.markerData.lng),
+                          title: "my marker",
+                          icon: new google.maps.MarkerImage(
+                                  vm.markerData.imgUrl,
+                                  null, /* size is determined at runtime */
+                                  null, /* origin is 0,0 */
+                                  null, /* anchor is bottom center of the scaled image */
+                                  new google.maps.Size(70, 70)
+                                )
+                      })
+      vm.marker.setMap(vm.map);
+      google.maps.event.addListener(vm.marker, 'click', function(){
+                      console.log("clicked");
+                  });
+      setInterval( function(){
+        vm.markerData.lat-=0.00001;
+        vm.markerData.lng-=0.00001;
+        // console.log(vm.markerData);
+        vm.marker.setPosition( new google.maps.LatLng(vm.markerData.lat, vm.markerData.lng) );
+
+    }, 60 );
+
+    };
+    mapInit();
+    console.dir(vm.map);
+    // window.navigator.geolocation.getCurrentPosition(
+    //   function (position) {
+    //     centerLatLng.lat = position.coords.latitude;
+    //     centerLatLng.lng = position.coords.longitude;
+    //     console.log(position);
+    //     mapInit();
+    //   },function (err) {
+    //     console.log(err);
+    //     mapInit();
+    //   }
+    // );
+
 
   }
 }());
