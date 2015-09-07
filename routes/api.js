4f137000d72835b553e22c9f8887f8a1ad4cd6c2
Imtaek Hong <imtaekh@gmail.com>
2015-09-06 19:00:38 -0700

    my_ig api created

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
+var XMLHttpRequest= require("xhr2");
+var express       = require('express');
+var router        = express.Router();
 
 router.route('/me')
   .get(function (req, res) {
     res.json({success:true, data:req.decoded});
   });
+router.route('/my_ig')
+  .get(function (req, res) {
+    var user = req.decoded;
+    getJSON("https://api.instagram.com/v1/users/"+user.instagramId+"/media/recent?count=10&access_token="+user.accessToken,
+      function (data) {
+        res.json({success:true, data:data.data});
+      },
+      function (status) {
+        res.json({success:false, message:status});
+      }
+    );
+  });
 
+function getJSON(url, successHandler, errorHandler) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('get', url, true);
+  xhr.onreadystatechange = function() {
+   var status;
+   var data;
+   // https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
+   if (xhr.readyState == 4) { // `DONE`
+     status = xhr.status;
+     if (status == 200) {
+       data = JSON.parse(xhr.responseText);
+       successHandler && successHandler(data);
+     } else {
+       errorHandler && errorHandler(status);
+     }
+   }
+  };
+  xhr.send();
+}
 module.exports = router;
